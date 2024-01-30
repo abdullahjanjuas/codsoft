93473ea0c18af230d3c45566e94828c1aca0347f
abdullahjanjuas <114680801+abdullahjanjuas@users.noreply.github.com>
2024-01-30 10:08:23 -0800

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,22 @@
+#A Password Generator using Python
+import random
+
+#Characters to be chosen from the string for password generation
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789()*&%$#@!"
+
+while True:
+    p_length = int(input("What length do you want for your password?"))
+    p_count = int(input("Hoe many passwords do you wish to generate?"))
+    for x in range(p_count):
+        password = ""
+        for x in range(p_length):
+            password_char = random.choice(chars)
+            password += password_char
+        print("Here is your password: " + password)
+    conti = input("\nDo you wish to continue? Y/N\n")
+    if(conti != 'Y'):
+        print("Bye!")
+        break 
+    
+        
+    
